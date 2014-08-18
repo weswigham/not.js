a425a9f6a8523e194cf5625494b17a99555c06d8
weswigham <wwigham@gmail.com>
2014-08-18 00:19:50 -0400

    refactoring, functioning, tests

--- index.js
+++ index.js
@@ -1,68 +1,115 @@
 var escape = require('escape-html');
 
-var buffer = "";
-function toAttr(obj) {
+function stringBuilder() {
+  this.buffer = "";
+};
+
+stringBuilder.prototype._toAttr = function(obj) {
   var ret = [];
   for (var key in obj) {
     if (obj.hasOwnProperty(key)) {
-      ret.push(""+key+"=\""+obj[key]+"\"");
+      if (obj[key] === null) {
+        ret.push(""+key);
+      } else {
+        ret.push(""+key+"=\""+obj[key]+"\"");
+      }
     }
   }
   return ret.join(' ');
 }
 
-var alternator = false;
-var xml = Proxy.create({ //TODO: Proxy as a function and let calling the context pass an object to expose as $scope
-  getPropertyDescriptor: function(key, rec) {
-    return {
-      value: (function() {
-        if (key === '$') {
-          return function(str, noEscape) {
-            var escape = escape;
-            if (noEscape) {
-              escape = function(s) {return s;};
-            }
-            if (typeof(str) === 'function') {
-              buffer += escape((/\/\*!?(?:\@preserve)?[ \t]*(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)\s*\*\//).exec(str.toString())[1]);
-            } else {
-              buffer += escape(str);
-            }
-          }
-        }
-        alternator = !alternator;
-        if (alternator) {
-          return; //Skip every other invocation - called twice for HasBinding and HasOwnBinding. Or something.
-        }
-        if (key.slice(-1) === '$') {
-          (buffer += "<"+key.slice(0,key.length-1)+" />");
-          return function(obj) {
-            var attr = toAttr(obj);
-            buffer = buffer.slice(0,buffer.length-3)
-            buffer += " " + attr + " />"
-            attr = null;
-          }
-        } else if (key.slice(0,1) === ('$')) {
-          (buffer += "</"+key.slice(1)+">");
-        } else {
-          (buffer += "<"+key+">");
-          return function(obj) {
-            var attr = toAttr(obj);
-            buffer = buffer.slice(0, buffer.length-1) +" "+ attr + ">"
-            attr = null;
-          }
-        }
-      })(), 
-      configurable: true
-    }
+stringBuilder.prototype.complete = function() {
+  return this.buffer;
+};
+
+stringBuilder.prototype.pushStartToken = function(name) {
+  this.buffer += "<"+name+">";
+};
+
+stringBuilder.prototype.rewriteStartTokenAttributes = function(obj) {
+  var attr = this._toAttr(obj);
+  this.buffer = this.buffer.slice(0, this.buffer.length-1) +" "+ attr + ">"
+};
+
+stringBuilder.prototype.pushEndToken = function(name) {
+  this.buffer += "</"+name+">";
+};
+
+stringBuilder.prototype.pushSingleToken = function(name) {
+  this.buffer += "<"+name+" />";
+};
+
+stringBuilder.prototype.rewriteSingleTokenAttributes = function(obj) {
+  var attr = this._toAttr(obj);
+  this.buffer = this.buffer.slice(0,this.buffer.length-3);
+  this.buffer += " " + attr + " />";
+};
+
+stringBuilder.prototype.pushRaw = function(str, noEscape) {
+  if (noEscape) {
+    this.buffer += str;
+  } else {
+    this.buffer += escape(str).replace(/(?:\r\n|\r|\n)/g, '<br />');
   }
-});
+};
+
+function jshtmlProxy(builder) {
+  var ret = function(scopeName, scope) {
+    if (!scope) {
+      scope = scopeName;
+      scopeName = "$scope";
+    }
+    var alternator = false;
+    return Proxy.create({
+      getPropertyDescriptor: function(key, rec) {
+        return {
+          value: (function() {
+            if (key === scopeName) {
+              return scope; 
+            }
+            if (key === '$') {
+              return function(str, noEscape) {
+                if (typeof(str) === 'function') {
+                  builder.pushRaw((/\/\*!?(?:\@preserve)?[ \t]*(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)\s*\*\//).exec(str.toString())[1], noEscape);
+                } else {
+                  builder.pushRaw(str, noEscape);
+                }
+              }
+            }
+            alternator = !alternator;
+            if (alternator) {
+              return; //Skip every other invocation - called twice for HasBinding and HasOwnBinding. Or something.
+            }
+            if (key.slice(-1) === '$') {
+              builder.pushSingleToken(key.slice(0,key.length-1));
+              return function(obj) {
+                builder.rewriteSingleTokenAttributes(obj);
+              }
+            } else if (key.slice(0,1) === ('$')) {
+              builder.pushEndToken(key.slice(1));
+            } else {
+              builder.pushStartToken(key);
+              return function(obj) {
+                builder.rewriteStartTokenAttributes(obj);
+              }
+            }
+          })(), 
+          configurable: true
+        }
+      }
+    });
+  };
+  
+  ret.collect = function() {
+    return builder.complete();
+  };
+  
+  return ret;
+}
 
 module.exports = {
-  buffer: "", //TODO: Output format builders
-  string: "",
-  xml: "",
-  dom: "",
-  create: function(builder) { //TODO: Create proxy with given builder
-  
+  string: stringBuilder, //TODO: Output format builders
+  create: function(builder) {
+    return jshtmlProxy((builder && new builder()) || new stringBuilder());
   }
 };
--- test/html.js
+++ test/html.js
@@ -1,31 +1,55 @@
-var jshtml = require('../index');
+﻿var jshtml = require('../index');
 require('chai').should();
 
 describe('the jshtml dsl', function() {
   it('is a consice way of representing html in javascript', function() {
-    var context = jshtml.create(jshtml.buffer);
-    with (context) {
-      html
-        head
-          meta$({title: "wat"})
-        $head
+  
+    var context = jshtml.create(jshtml.string); //Get a dsl context object
+    var scope = { //Make a scope to allow calling within the dsl
+      paragraph: "Paragraph content!!"
+    };
+    //Call the context with the name you want the scope accessible by and the scope object itself
+    //"$scope" is the default and may be omitted
+    with (context("$scope", scope)) {
+    
+      //Any valid identifier is a used as a tag (with some exceptions, see below)
+      html 
+        head 
+          
+          //Set attributes by 'calling' the tag with them (does not function on closing tags, obviously)
+          meta$({title: "wat"}) //Identifiers ending with a $ are 'self-closing' tags
+        $head //Identifiers starting with a $ are closing tags
         body({style: "width: 100%;"})
+          //Use the $ function to make a text node
           p; h1({style: "text-align: right;"}); $("Title text"); $h1 //Semicolons allow for multiple on one line
-            $("Paragraph content!!")
+            $($scope.paragraph) //Access scoped variables that won't be intercepted by the dsl using the specified name
           $p
           hr$
           p
-            $("Escaped <html> & such.")
+            //Disable automatic escaping of text nodes, (this lets you use partials!)
+            $("Unescaped html <strong> and such. </strong>", true)
           $p
-          p
+          p //Built in easy multiline 'string' support (and newlines are auto replaced with 'br's if it's escaped)
             $(function() {/*
               Or long form
               for all the content.
             */})        
           $p
+          
+          //Include plain js logic inline with your template!
+          for ($scope.i=0; $scope.i<4; $scope.i++) {
+            p
+              $("I'm number "+$scope.i+"!");
+            $p
+          }
         $body
       $html
     }
-    context.collect().should.not.be.null;
+    context.collect().should.be.equal(
+'<html><head><meta title=\"wat\" /></head><body style=\"width: 100%;\"><p><h1 style=\"text-align: right;\">Title '+
+'text</h1>Paragraph content!!</p><hr /><p>Unescaped html <strong> and such. </strong></p><p>              Or long form<br'+
+' />              for all the content.</p><p>I&#39;m number 0!</p><p>I&#39;m number 1!</p><p>I&#39;m number 2!</p><p>I&#3'+
+'9;m number 3!</p></body></html>'
+    );
   });
 });

ecb4aab69c91278565f3a91ffad7f02047d32c9f
Wesley Wigham <wwigham@gmail.com>
2016-03-15 19:19:21 -0400

    Speculatively fix for new proxy trap names

--- not.js
+++ not.js
@@ -261,6 +261,7 @@ function jshtmlProxy(builder) {
     };
     var proxy = makeProxy({ //TODO: Alternate implementation for the newer harmony proxy API supported by Firefox
       getPropertyDescriptor: function(key) {return {value: true, configurable: true}}, //We are all the properties!
+      has: function(target, key) { return true; }, // New version of the has-all-the-things trap
       get: function(rec, key) {
         return (function() {
             if (key === scopeName) {
